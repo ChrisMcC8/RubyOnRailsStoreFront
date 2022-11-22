f6a23d081e57f4e6224159227bc85a08b2c76e0a
ChrisMcC8 <chris.mccallum97@gmail.com>
2022-11-22 11:28:15 -0600

    Initial

---
+++ app/views/products/_product.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! product, :id, :id, :name, :manufacturer, :price, :stock, :description, :weight, :categoryId, :created_at, :updated_at
+json.url product_url(product, format: :json)
---
+++ db/migrate/20221121214457_create_products.rb
@@ -0,0 +1,15 @@
+class CreateProducts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.string :manufacturer
+      t.decimal :price
+      t.integer :stock
+      t.text :description
+      t.decimal :weight
+      t.integer :categoryId
+
+      t.timestamps
+    end
+  end
+end
